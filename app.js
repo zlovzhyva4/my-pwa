6b24a9b3d3612d7ec1ff20d293c1e156aaa03929
zlovzhyva4 <65289465+zlovzhyva4@users.noreply.github.com>
2026-02-04 10:33:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert('JS працює');
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.getElementById('main-container');
+  const tasks = document.getElementById('tasks-container');
+
+  const showBtn = document.getElementById('show-tasks-btn');
+  const backBtn = document.getElementById('back-btn');
+
+  showBtn.addEventListener('click', () => {
+    main.style.display = 'none';
+    tasks.style.display = 'block';
+  });
+
+  backBtn.addEventListener('click', () => {
+    tasks.style.display = 'none';
+    main.style.display = 'block';
+  });
+});
